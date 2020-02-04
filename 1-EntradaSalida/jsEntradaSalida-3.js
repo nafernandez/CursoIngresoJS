df76dee8d198332adff9cd594adead94a2d654fb
nafernandez <60605555+nafernandez@users.noreply.github.com>
2020-02-04 19:12:39 -0300

    Ejercicio 3- Document

--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -2,7 +2,18 @@
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+    //var nombre;
+    //nombre = document.getElementById("elNombre").value;
+    //nombre = document.getElementById("elNombre").value = "Mauricio";
+    //alert(nombre);
+
+    //Ejercicio: 3
+   // var valorEntrada;
+    //valorEntrada = document.getElementById("elNombre").value;
+    //alert(valorEntrada);
+    var nombre;
+    nombre= document.getElementById('elNombre'). value;
+    alert(nombre);    
 }
 
 
