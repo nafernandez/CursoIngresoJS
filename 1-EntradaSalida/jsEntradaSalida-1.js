a4b0ae834b00ed20d1c4d1ed122dd803ce63456d
nafernandez <60605555+nafernandez@users.noreply.github.com>
2020-02-04 19:06:15 -0300

    Mi primer commit
    Mi primer commit

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,9 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
 function mostrar()
 {
-	alert("ok");
+	//var nombre;
+	//nombre= prompt ("Hola", "NN");
+	//alert(nombre);
+	alert ("Esto funciona de maravilla");
 }
 
