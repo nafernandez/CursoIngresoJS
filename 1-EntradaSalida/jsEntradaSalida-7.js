70fdf66808972b8312070ed8ae224c9da3a12536
Natalia Fernández <60605555+nafernandez@users.noreply.github.com>
2020-02-08 15:54:30 -0300

    Ejercicio 7- hecho.
    Resta, suma, multiplicación y división.

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,70 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
+	var primerNumero;
+	var segundoNumero;
+	var suma;
+
+	primerNumero=document.getElementById('numeroUno').value;
+	segundoNumero=document.getElementById('numeroDos').value;
+
+	primerNumero=parseInt(primerNumero);
+	segundoNumero=parseInt(segundoNumero);
+
+	suma= primerNumero+segundoNumero;
+
+	alert(suma);
 	
 }
 
 function restar()
 {
+	var primerNumero;
+	var segundoNumero;
+	var resta;
+
+	primerNumero=document.getElementById('numeroUno').value;
+	segundoNumero=document.getElementById('numeroDos').value;
+
+	primerNumero=parseInt(primerNumero);
+	segundoNumero=parseInt(segundoNumero);
+
+	resta=primerNumero-segundoNumero;
+
+	alert(resta);
 	
 }
 
 function multiplicar()
 { 
-	
+	var primerNumero;
+	var segundoNumero;
+	var multiplicacion;
+
+	primerNumero=document.getElementById('numeroUno').value;
+	segundoNumero=document.getElementById('numeroDos').value;
+
+	multiplicacion=primerNumero*segundoNumero;
+
+	alert(multiplicacion);
+
 }
 
 function dividir()
 {
+	var primerNumero;
+	var segundoNumero;
+	var division;
+
+	primerNumero=document.getElementById('numeroUno').value;
+	segundoNumero=document.getElementById('numeroDos').value;
+
+	primerNumero=parseInt(primerNumero);
+	segundoNumero=parseInt(segundoNumero);
+
+	division= primerNumero/segundoNumero;
+
+	alert(division);
 	
 }
 
