41fa9d1b70573d1faff99d8e8f860c41a2ef34d3
Natalia Fernández <60605555+nafernandez@users.noreply.github.com>
2020-02-08 15:59:48 -0300

    Ejercicio 9 - mostrarAumento, hecho.

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,35 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	var importeSueldo;
+	var aumento;
+	var resultadoAumento;
+
+
+	importeSueldo=document.getElementById('sueldo').value;
+	importeSueldo=parseInt(importeSueldo);
+
+	aumento= importeSueldo*10/100;
+
+
+
+	resultadoAumento=importeSueldo+aumento;
+
+	document.getElementById('resultado').value=resultadoAumento;
+
+	
+
+	resultadoAumento=importeSueldo+aumento;
+
+	console.log(resultadoAumento);
+
+	
+
+	
+
+
+	
+
 
 	
 }
