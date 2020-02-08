ffd5efb2c62406416b98c839eeec3682b41f8e95
Natalia Fernández <60605555+nafernandez@users.noreply.github.com>
2020-02-08 16:01:11 -0300

    Ejercicio 10 - mostrar descuento, hecho.

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,19 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
+	var importeInicial;
+	var importeDescuento;
+	var descuento;
+
+	importeInicial=document.getElementById('importe').value;
+	importeInicial=parseInt(importeInicial);
+
+	descuento=importeInicial*25/100;
+
+	importeDescuento= importeInicial-descuento;
+
+	document.getElementById('resultado').value=importeDescuento;
+
+	console.log(importeDescuento);
 	
 }
