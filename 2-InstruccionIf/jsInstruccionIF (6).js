a326574debf3f56dd84554308f3a574bae5205a2
Natalia Fernández <60605555+nafernandez@users.noreply.github.com>
2020-02-10 22:03:34 -0300

    Ejercicio 6- hecho.

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,44 @@
 function mostrar()
 {
-//tomo la edad  
+	var edad;
+	edad=document.getElementById('edad').value;
 
+	if(edad>18)
+		{alert("es un adulto");
+		}
+	else
+		if(edad<13)
+		{alert("es un niño");
+		}
+		else
+			
+		{alert("es un adolescente");
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//tomo la edad  
+//var edad;
+//edad = document.getElementById("edad").value;
+//if(edad >= 13 && edad <= 17)
+  //  alert("Es adolescente");
+//else if(edad >= 18)
+  //      alert("Mayor de edad");
+    //else
+      //  alert("Menor de edad");
 
 
 
 }//FIN DE LA FUNCIÓN
+
