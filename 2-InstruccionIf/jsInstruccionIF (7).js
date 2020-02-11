755d861457207111f917f0f916ac4d595aa9aee2
Natalia Fernández <60605555+nafernandez@users.noreply.github.com>
2020-02-10 22:04:11 -0300

    Ejercicio 7 - hecho.

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,8 +1,31 @@
 function mostrar()
 {
+	var edad;
+	var estado;
+
+	edad=document.getElementById('edad').value;
+	estado=document.getElementById('estadoCivil').value;
+
+	if(edad<17 && estado!="Soltero")
+		{alert("Es muy pequeño para no ser soltero");
+	}
+		
+
+
+
+
+
+
+
+
+
+
 //tomo la edad  
-
-	
-
-
+//var edad;
+//edad = document.getElementById("edad").value;
+//var estado;
+//estado = document.getElementById("estadoCivil").value;
+//if((edad < 18) && (estado != "Soltero"))
+  //  alert("Es muy pequeño para no ser soltero");
+    
 }//FIN DE LA FUNCIÓN
